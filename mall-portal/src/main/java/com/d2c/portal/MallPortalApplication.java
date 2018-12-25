59e8f82ca06fc7f6857c9786149b0e06101dfe86
BaiCai <709931138@qq.com>
2018-12-25 16:21:56 +0800

    加入Zuul支持

--- mall-portal/src/main/java/com/d2c/portal/MallPortalApplication.java
+++ mall-portal/src/main/java/com/d2c/portal/MallPortalApplication.java
@@ -3,11 +3,13 @@ package com.d2c.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableZuulProxy
 public class MallPortalApplication {
 
     public static void main(String[] args) {
