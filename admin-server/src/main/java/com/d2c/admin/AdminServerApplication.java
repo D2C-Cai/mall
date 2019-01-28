c2025d88ca34034b15046eda544edacc7e297348
BaiCai <709931138@qq.com>
2019-01-28 17:19:56 +0800

    增加SpringBoot-Admin2.0监控

---
+++ admin-server/src/main/java/com/d2c/admin/AdminServerApplication.java
@@ -0,0 +1,16 @@
+package com.d2c.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
+
---
+++ admin-server/src/test/java/com/d2c/admin/AdminServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.d2c.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
